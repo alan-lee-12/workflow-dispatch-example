ba3f9baa4d610b951a97e17aacf1745524fe2a83
Alan Lee <alan.lee@getethos.com>
2021-06-28 22:02:42 -0700

    Initial commit: add workflow file, script, and empty payload.json.

---
+++ index.js
@@ -0,0 +1,7 @@
+// How do I get the payload from the API call that triggers the worflow?
+
+// May be able to write the json payload to file, and this JS script can read from it.
+
+const payload = require('payload.json')
+
+console.log(`Here's the payload: ${payload}`)
